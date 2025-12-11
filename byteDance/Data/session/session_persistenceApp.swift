103aedb924ab447f14381eaa5463f423ae8a9e10
fflying666 <1284835369@qq.com>
2025-12-11 20:01:43 +0800

    Create session_persistenceApp.swift

---
+++ byteDance/Data/session/session_persistenceApp.swift
@@ -0,0 +1,17 @@
+//
+//  session_and_data_persistenceApp.swift
+//  session and data persistence
+//
+//  Created by bluefly on 2025/12/3.
+//
+
+import SwiftUI
+
+@main
+struct session_and_data_persistenceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
