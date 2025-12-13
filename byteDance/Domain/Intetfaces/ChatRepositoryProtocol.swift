a6c0f55a15295eed73377a8d70dca5b47541f730
fflying666 <1284835369@qq.com>
2025-12-13 21:08:54 +0800

    会话删除与重命名

--- byteDance/Domain/Intetfaces/ChatRepositoryProtocol.swift
+++ byteDance/Domain/Intetfaces/ChatRepositoryProtocol.swift
@@ -18,4 +18,5 @@ public protocol ChatRepositoryProtocol {
     //新增
     func saveSessions(_ sessions: [Session])  // 保存会话列表（含状态）
     func setCurrentSession(id: UUID)          // 标记当前会话
+    func deleteSession(id: UUID)// 删除会话的方法
 }
