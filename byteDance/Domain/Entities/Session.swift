53bc87408c6952cee07d75244f3f81c34e79aae5
fflying666 <1284835369@qq.com>
2025-12-13 18:08:51 +0800

    Update Session.swift
    新增持久化

--- byteDance/Domain/Entities/Session.swift
+++ byteDance/Domain/Entities/Session.swift
@@ -11,11 +11,22 @@ public struct Session: Codable, Equatable, Identifiable {
     public var title: String
     public var messages: [Message]
     public var archived: Bool
+    // 新增状态字段
+    public var isCurrent: Bool = false // 是否为当前活跃会话
+    
+     public init(
+         id: UUID = UUID(),
+         title: String,
+         messages: [Message] = [],
+         archived: Bool = false,
+         isCurrent: Bool = false,
+         unreadCount: Int = 0
+     ) {
+         self.id = id
+         self.title = title
+         self.messages = messages
+         self.archived = archived
+         self.isCurrent = isCurrent
 
-    public init(id: UUID = UUID(), title: String, messages: [Message] = [], archived: Bool = false) {
-        self.id = id
-        self.title = title
-        self.messages = messages
-        self.archived = archived
-    }
+     }
 }
